48dcd6fada3495f1211bf491de5e86b50ed18b6c
Simon Lynch <simon.lynch@hashicorp.com>
2023-03-28 09:43:09 +1100

    update for egress vault

--- security_group.tf
+++ security_group.tf
@@ -50,3 +50,15 @@ resource "aws_security_group_rule" "egress_dns_udp" {
 
   security_group_id = aws_security_group.agents.id
 }
+
+
+resource "aws_security_group_rule" "egress_vault_tcp" {
+  type        = "egress"
+  from_port   = 8200
+  to_port     = 8200
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  description = "Allow Vault traffic egress."
+
+  security_group_id = aws_security_group.agents.id
+}
